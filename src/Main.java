4ec06e76ae29037b743071ec8e931d81cc0fc632
cihaai <78753548+cihaai@users.noreply.github.com>
2021-02-10 09:24:34 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main 
+{
+   public static void main(String[] args) 
+   {
+    String a = "Peter";
+    int vowels = 0;
+    
+    for (int i = 0; i < a.length(); ++i) {
+      char ch = a.charAt(i);
+          
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ) {
+    	++vowels;
+    }
+    System.out.println("Vowels: " + vowels);
+    }
+   }
+}
